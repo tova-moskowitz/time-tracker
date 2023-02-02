c6683ba4829f6b723c794a03bed311cc03bcc051
= <=>
2023-02-02 11:43:15 -0500

    add profile pic

--- src/components/UserProfileCard.js
+++ src/components/UserProfileCard.js
@@ -1,8 +1,10 @@
 import "../style/App.css";
+import ProfilePic from "../images/image-jeremy.png";
 
 function UserProfileCard({ user }) {
   return (
     <div className="profile">
+      <img className="profilePic" src={ProfilePic} alt="" />
       <p>Report For:</p>
       {user}
       <div className="timeframes">
