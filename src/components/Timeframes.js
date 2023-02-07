52834e9f64f0754ea16eb8d6fcf2428d40165e60
= <=>
2023-02-06 20:38:17 -0500

    fix margin of cards grid, it was bigger on one side than the other

--- src/components/Timeframes.js
+++ src/components/Timeframes.js
@@ -4,40 +4,38 @@ import cn from "classnames";
 
 function Timeframes({ clickHandler, activeTimeframe }) {
   return (
-    <>
-      <div className="timeframes">
-        <p
-          onClick={() => {
-            clickHandler("daily");
-          }}
-          className={cn("daily", {
-            active: activeTimeframe === "daily",
-          })}
-        >
-          Daily
-        </p>
-        <p
-          onClick={() => {
-            clickHandler("weekly");
-          }}
-          className={cn("weekly", {
-            active: activeTimeframe === "weekly",
-          })}
-        >
-          Weekly
-        </p>
-        <p
-          onClick={() => {
-            clickHandler("monthly");
-          }}
-          className={cn("monthly", {
-            active: activeTimeframe === "monthly",
-          })}
-        >
-          Monthly
-        </p>
-      </div>
-    </>
+    <div className="timeframes">
+      <p
+        onClick={() => {
+          clickHandler("daily");
+        }}
+        className={cn("daily", {
+          active: activeTimeframe === "daily",
+        })}
+      >
+        Daily
+      </p>
+      <p
+        onClick={() => {
+          clickHandler("weekly");
+        }}
+        className={cn("weekly", {
+          active: activeTimeframe === "weekly",
+        })}
+      >
+        Weekly
+      </p>
+      <p
+        onClick={() => {
+          clickHandler("monthly");
+        }}
+        className={cn("monthly", {
+          active: activeTimeframe === "monthly",
+        })}
+      >
+        Monthly
+      </p>
+    </div>
   );
 }
 
